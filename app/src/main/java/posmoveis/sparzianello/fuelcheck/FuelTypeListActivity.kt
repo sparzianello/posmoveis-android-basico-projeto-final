0e74edbf836808f0a51b356e7779268c716e8a2a
Samuel Parzianello <parzianellosamuel2@gmail.com>
2025-03-23 22:23:11 -0300

    commit complementar 2
    Commit complementar contendo:
    
    - BugFix:
      - Renomeado nome de itens no string.xml para melhor entendimento
      - Renomeado descrição de itens no string.xml para melhorar apresentação na tela
    
    - Features:
      - Criado Activity (.kt e .xml) para apresentação dos combustíveis ao usuário
      - Adicionado no string.xml combustíveis a serem carregados na Activity criada
      - Removido método teste da MainActivity.kt (para trocar hint de acordo com clique)
      - Criado métodos reais para alterar os hints de acordo com o que for selecionado na Activity criada

---
+++ app/src/main/java/posmoveis/sparzianello/fuelcheck/FuelTypeListActivity.kt
@@ -0,0 +1,31 @@
+package posmoveis.sparzianello.fuelcheck
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import posmoveis.sparzianello.fuelcheck.databinding.ActivityFuelTypeListBinding
+
+class FuelTypeListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFuelTypeListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFuelTypeListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fuelType = intent.getIntExtra("fuelType", 0)
+
+        binding.lvFuelTypes.setOnItemClickListener { _, _, position, _ ->
+
+            val fuelTypes = resources.getStringArray(R.array.fuel)
+            val selectedItemDescription = fuelTypes[position]
+
+            intent.putExtra("itemDescription", selectedItemDescription)
+            intent.putExtra("fuelTypeRequested", fuelType)
+
+            setResult(RESULT_OK, intent)
+
+            finish()
+        }
+    }
+}
--- app/src/main/java/posmoveis/sparzianello/fuelcheck/MainActivity.kt
+++ app/src/main/java/posmoveis/sparzianello/fuelcheck/MainActivity.kt
@@ -1,6 +1,8 @@
 package posmoveis.sparzianello.fuelcheck
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import posmoveis.sparzianello.fuelcheck.databinding.ActivityMainBinding
 
@@ -14,15 +16,43 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.editFuelType1Consumption.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                binding.inputFuelType1Consumption.hint = getString(R.string.app_name)
-                binding.inputFuelType1Price.hint = getString(R.string.fuel_value)
-            } else {
-                binding.inputFuelType1Price.hint = getString(R.string.app_name)
-                binding.inputFuelType1Consumption.hint = getString(R.string.consumption)
+        binding.buttonSearchFuelType1.setOnClickListener {
+            getFuelType.launch(
+                Intent(
+                    this,
+                    FuelTypeListActivity::class.java
+                ).apply { putExtra("fuelType", 1) })
+        }
+        binding.buttonSearchFuelType2.setOnClickListener {
+            getFuelType.launch(
+                Intent(
+                    this,
+                    FuelTypeListActivity::class.java
+                ).apply { putExtra("fuelType", 2) })
+        }
+    }
+
+    private val getFuelType =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val fuelSelected = it.data?.getStringExtra("itemDescription")
+                val fuelType = it.data?.getIntExtra("fuelTypeRequested", 0)
+
+                when (fuelType) {
+                    1 -> {
+                        binding.inputFuelType1Consumption.hint =
+                            getString(R.string.consumption).plus(" ").plus(fuelSelected)
+                        binding.inputFuelType1Price.hint =
+                            getString(R.string.fuel_price).plus(" ").plus(fuelSelected)
+                    }
+
+                    else -> {
+                        binding.inputFuelType2Consumption.hint =
+                            getString(R.string.consumption).plus(" ").plus(fuelSelected)
+                        binding.inputFuelType2Price.hint =
+                            getString(R.string.fuel_price).plus(" ").plus(fuelSelected)
+                    }
+                }
             }
         }
-
-    }
 }
